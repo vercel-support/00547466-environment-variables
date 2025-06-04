53182756009a0f80f58409e1ea6a54ebc1d805f7
Omer Boysal <oboysaldev@gmail.com>
2025-06-04 11:28:02 +1000

    Initial commit

---
+++ postbuild.js
@@ -0,0 +1,4 @@
+console.log("--- POSTBUILD ENV VARIABLES ---");
+console.log("VERCEL:", process.env.VERCEL);
+console.log("CI:", process.env.CI);
+console.log("VERCEL_ENV:", process.env.VERCEL_ENV);
